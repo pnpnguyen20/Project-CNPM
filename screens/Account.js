571f5852273ae5b66f3c81aa9f310f25e52f547b
Tran Quang Thinh <tqthinh20@clc.fitus.edu.vn>
2022-07-05 22:35:19 +0700

    Update Account.js

--- screens/Account.js
+++ screens/Account.js
@@ -1,17 +1,59 @@
-import { View, Button, Text, Image, StyleSheet, SafeAreaView} from "react-native";
+import { View, Button, TouchableOpacity, Text, Image, StyleSheet, SafeAreaView, ScrollView} from "react-native";
 import React from "react";
 import colors from "../constants/colors";
+
 
 const Account = () => {
   return (
     <SafeAreaView style={{ backgroundColor: colors.mainBackground, flex: 1 }}>
-      <View style = {{paddingBottom: 25, paddingHorizontal: 30}}>
-        <Text style = {{fontSize: 24, fontWeight: 'bold', color: 'white'}}>Trần Quang Thịnh </Text>
-        <Text style = {{fontSize: 14, lineHeight: 14, fontWeight: '500', color: 'white'}}>@tqthinh20</Text>
-      </View>
+      <ScrollView
+        showsVerticalScrollIndicator = {false}
+        contentContainerStyle = {{paddingBottom:30}}>
+          <View style = {{marginTop: 29, paddingHorizontal: 29}}>
+            <Text style = {{fontSize: 24, fontWeight: 'bold', color: 'white'}}>Trần Quang Thịnh </Text>
+            <Text style = {{fontSize: 14, lineHeight: 14, fontWeight: '500', color: 'white'}}>tqthinh20@clc.fitus.edu.vn</Text>
+          </View>
+          
+          <View style={styles.menuWrapper}>
+            <TouchableOpacity onPress={() => {}}>
+              <View style={styles.menuItem}>
+                <Text style={styles.menuItemText}>Edit information</Text>
+              </View>
+            </TouchableOpacity>
+        
+            <TouchableOpacity onPress={() => {}}>
+              <View style={styles.menuItem}>
+                <Text style={styles.menuItemText}>Settings</Text>
+              </View>
+            </TouchableOpacity>
+        
+            <TouchableOpacity onPress={() => {}}>
+              <View style={styles.menuItem}>
+                <Text style={styles.menuItemText}>Log out</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+      </ScrollView> 
     </SafeAreaView>
   );
 };
 
 
 export default Account;
+const styles = StyleSheet.create({
+  menuWrapper: {
+    marginTop: 10,
+  },
+  menuItem: {
+    flexDirection: 'row',
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+  },
+  menuItemText: {
+    color: '#777777',
+    marginLeft: 20,
+    fontWeight: '600',
+    fontSize: 16,
+    lineHeight: 26,
+  },
+});
