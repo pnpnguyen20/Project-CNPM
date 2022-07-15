672913cebdf0c8c4a855e81b8c0ba854eb6a4e4c
sukivn <ndbao202@clc.fitus.edu.vn>
2022-07-15 17:04:51 +0700

    2nd

---
+++ backend/Project.js
@@ -0,0 +1,11 @@
+class Project {
+    constructor(proj_id, proj_name, proj_createday, proj_deadline, proj_status, proj_admin, proj_owner) {
+        this.proj_id = proj_id
+        this.proj_name = proj_name
+        this.proj_createday = proj_createday
+        this.proj_deadline = proj_deadline
+        this.proj_status = proj_status
+        this.proj_admin = proj_admin
+        this.proj_owner = proj_owner
+    }
+}
---
+++ backend/Task.js
@@ -0,0 +1,5 @@
+class Task {
+    constructor(proj_id, task_id, task_name, task_status, task_description, task_createday, task_deadline, task_last_edit_time, task_last_editor, task_creator) {
+
+    }
+}
