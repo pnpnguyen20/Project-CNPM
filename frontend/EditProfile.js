d075ac1172238dc940cbf81efcdba17605d5c7f6
Tran Quang Thinh <tqthinh20@clc.fitus.edu.vn>
2022-07-17 22:59:25 +0700

    Edit & Connect Screens (Account)

--- frontend/EditProfile.js
+++ frontend/EditProfile.js
@@ -1,228 +1,105 @@
 import React, {useState} from 'react';
 import { View, Text, TouchableOpacity, ImageBackground, TextInput, StyleSheet} from 'react-native';
 
-import {useTheme} from 'react-native-paper';
-
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Feather from 'react-native-vector-icons/Feather';
 
-import BottomSheet from 'reanimated-bottom-sheet';
-import Animated from 'react-native-reanimated';
+import colors from "../constants/colors";
 
-import ImagePicker from 'react-native-image-crop-picker';
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const EditProfileScreen = () => {
+const EditProfile = ({navigation}) => {
 
   const [image, setImage] = useState('https://www.kindpng.com/picc/m/78-785827_user-profile-avatar-login-account-male-user-icon.pngg');
-  const {colors} = useTheme();
-
-  const takePhotoFromCamera = () => {
-    ImagePicker.openCamera({
-      compressImageMaxWidth: 300,
-      compressImageMaxHeight: 300,
-      cropping: true,
-      compressImageQuality: 0.7
-    }).then(image => {
-      console.log(image);
-      setImage(image.path);
-      this.bs.current.snapTo(1);
-    });
-  }
-
-  const choosePhotoFromLibrary = () => {
-    ImagePicker.openPicker({
-      width: 300,
-      height: 300,
-      cropping: true,
-      compressImageQuality: 0.7
-    }).then(image => {
-      console.log(image);
-      setImage(image.path);
-      this.bs.current.snapTo(1);
-    });
-  }
-
-  renderInner = () => (
-    <View style={styles.panel}>
-      <View style={{alignItems: 'center'}}>
-        <Text style={styles.panelTitle}>Upload Photo</Text>
-        <Text style={styles.panelSubtitle}>Choose Your Profile Picture</Text>
-      </View>
-      <TouchableOpacity style={styles.panelButton} onPress={takePhotoFromCamera}>
-        <Text style={styles.panelButtonTitle}>Take Photo</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.panelButton} onPress={choosePhotoFromLibrary}>
-        <Text style={styles.panelButtonTitle}>Choose From Library</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.panelButton}
-        onPress={() => this.bs.current.snapTo(1)}>
-        <Text style={styles.panelButtonTitle}>Cancel</Text>
-      </TouchableOpacity>
-    </View>
-  );
-
-  renderHeader = () => (
-    <View style={styles.header}>
-      <View style={styles.panelHeader}>
-        <View style={styles.panelHandle} />
-      </View>
-    </View>
-  );
-
-  bs = React.createRef();
-  fall = new Animated.Value(1);
 
   return (
-    <View style={styles.container}>
-      <BottomSheet
-        ref={this.bs}
-        snapPoints={[330, 0]}
-        renderContent={this.renderInner}
-        renderHeader={this.renderHeader}
-        initialSnap={1}
-        callbackNode={this.fall}
-        enabledGestureInteraction={true}
-      />
-      <Animated.View style={{margin: 20,
-        opacity: Animated.add(0.1, Animated.multiply(this.fall, 1.0)),
-    }}>
-        <View style={{alignItems: 'center'}}>
-          <TouchableOpacity onPress={() => this.bs.current.snapTo(0)}>
-            <View
-              style={{
-                height: 100,
-                width: 100,
-                borderRadius: 15,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <ImageBackground
-                source={{
-                  uri: image,
-                }}
-                style={{height: 100, width: 100}}
-                imageStyle={{borderRadius: 15}}>
-                <View
-                  style={{
-                    flex: 1,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                  }}>
-                  <Icon
-                    name="camera"
-                    size={35}
-                    color="#fff"
-                    style={{
-                      opacity: 0.7,
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                      borderWidth: 1,
-                      borderColor: '#fff',
-                      borderRadius: 10,
-                    }}
-                  />
-                </View>
-              </ImageBackground>
-            </View>
-          </TouchableOpacity>
-          <Text style={{marginTop: 10, fontSize: 18, fontWeight: 'bold'}}>
-            John Doe
-          </Text>
-        </View>
-
+    <SafeAreaView style={styles.container}>
+      <View style = {{marginLeft: 30, marginRight:30}}>
         <View style={styles.action}>
-          <FontAwesome name="user-o" color={colors.text} size={20} />
+          <FontAwesome name="user-o" color={colors.inactive} size={20} />
           <TextInput
-            placeholder="First Name"
-            placeholderTextColor="#666666"
+            placeholder="Name"
+            placeholderTextColor="#dcdcdc"
             autoCorrect={false}
             style={[
               styles.textInput,
               {
-                color: colors.text,
+                color: "white",
               },
             ]}
           />
         </View>
         <View style={styles.action}>
-          <FontAwesome name="user-o" color={colors.text} size={20} />
-          <TextInput
-            placeholder="Last Name"
-            placeholderTextColor="#666666"
-            autoCorrect={false}
-            style={[
-              styles.textInput,
-              {
-                color: colors.text,
-              },
-            ]}
-          />
-        </View>
-        <View style={styles.action}>
-          <Feather name="phone" color={colors.text} size={20} />
+          <Feather name="phone" color={colors.inactive} size={20} />
           <TextInput
             placeholder="Phone"
-            placeholderTextColor="#666666"
+            placeholderTextColor="#dcdcdc"
             keyboardType="number-pad"
             autoCorrect={false}
             style={[
               styles.textInput,
               {
-                color: colors.text,
+                color: "white",
               },
             ]}
           />
         </View>
         <View style={styles.action}>
-          <FontAwesome name="envelope-o" color={colors.text} size={20} />
+          <FontAwesome name="envelope-o" color={colors.inactive} size={20} />
           <TextInput
             placeholder="Email"
-            placeholderTextColor="#666666"
+            placeholderTextColor="#dcdcdc"
             keyboardType="email-address"
             autoCorrect={false}
             style={[
               styles.textInput,
               {
-                color: colors.text,
+                color: "white",
               },
             ]}
           />
         </View>
         <View style={styles.action}>
-          <FontAwesome name="globe" color={colors.text} size={20} />
+          <FontAwesome name="calendar" color={colors.inactive} size={20} />
           <TextInput
-            placeholder="Country"
-            placeholderTextColor="#666666"
+            placeholder="Birth date"
+            placeholderTextColor="#dcdcdc"
             autoCorrect={false}
             style={[
               styles.textInput,
               {
-                color: colors.text,
+                color: "white",
               },
             ]}
           />
         </View>
         <View style={styles.action}>
-          <Icon name="map-marker-outline" color={colors.text} size={20} />
+          <Icon name="map-marker-outline" color={colors.inactive} size={20} />
           <TextInput
             placeholder="Address"
-            placeholderTextColor="#666666"
+            placeholderTextColor="#dcdcdc"
             autoCorrect={false}
             style={[
               styles.textInput,
               {
-                color: colors.text,
+                color: "white",
               },
             ]}
           />
         </View>
-        <TouchableOpacity style={styles.commandButton} onPress={() => {}}>
-          <Text style={styles.panelButtonTitle}>Submit</Text>
-        </TouchableOpacity>
-      </Animated.View>
+        <View style = {{flexDirection: "column", alignSelf: "center", marginVertical: 30}}>
+          <TouchableOpacity style={styles.commandButton} onPress={() => navigation.navigate("BotTab")}>
+            <Text style={styles.panelButtonTitle}>Confirm</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.commandButton} onPress={() => navigation.navigate("BotTab")}>
+            <Text style={styles.panelButtonTitle}>Cancel</Text>
+          </TouchableOpacity>
+        </View>
+        
     </View>
+    </SafeAreaView>
   );
 };
 
@@ -231,55 +108,16 @@ export default EditProfile;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 70,
+    backgroundColor: colors.mainBackground
   },
   commandButton: {
-    padding: 15,
+    paddingHorizontal: 100,
+    paddingVertical: 15,
     borderRadius: 10,
-    backgroundColor: '#FF6347',
+    backgroundColor: colors.primary,
     alignItems: 'center',
     marginTop: 10,
-  },
-  panel: {
-    padding: 20,
-    backgroundColor: '#FFFFFF',
-    paddingTop: 20,
-  },
-  header: {
-    backgroundColor: '#FFFFFF',
-    shadowColor: '#333333',
-    shadowOffset: {width: -1, height: -3},
-    shadowRadius: 2,
-    shadowOpacity: 0.4,
-    paddingTop: 20,
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-  },
-  panelHeader: {
-    alignItems: 'center',
-  },
-  panelHandle: {
-    width: 40,
-    height: 8,
-    borderRadius: 4,
-    backgroundColor: '#00000040',
-    marginBottom: 10,
-  },
-  panelTitle: {
-    fontSize: 27,
-    height: 35,
-  },
-  panelSubtitle: {
-    fontSize: 14,
-    color: 'gray',
-    height: 30,
-    marginBottom: 10,
-  },
-  panelButton: {
-    padding: 13,
-    borderRadius: 10,
-    backgroundColor: '#FF6347',
-    alignItems: 'center',
-    marginVertical: 7,
   },
   panelButtonTitle: {
     fontSize: 17,
