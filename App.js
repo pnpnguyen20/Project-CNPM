0acac1f88144a8704b04ed5cc63f31ef8feaa965
pnpnguyen20 <pnpnguyen20@clc.fitus.edu.vn>
2022-07-23 09:19:45 +0700

    fix bug luu y khi push

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { React, useState } from "react";
-import {StatusBar} from "react-native";
+import { StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import BotTab from "./navigation/BotTab";
 import StartScreen from "./navigation/StartScreen";
@@ -8,22 +8,24 @@ export default function App(props) {
   // const Stack = createNativeStackNavigator();
 
   return (
-    
+
     <NavigationContainer>
-      
-        {/* <Stack.Navigator
+      <BotTab />
+      {/* <Stack.Navigator
         initialRouteName="BotTab"
         screenOptions={{
           headerShown: false,
           // headerBackButtonMenuEnabled: true,
         }}
       >
-        <Stack.Screen name={"BotTab"} component={BotTab} />
-      </Stack.Navigator>  */}
-      {/* <StatusBar barStyle= 'light-content'/> */}
-      <StartScreen/>
-      {/* <BotTab/> */}
-      
+        <BotTab />
+        
+      </Stack.Navigator> */}
+      {/* <Stack.Screen name={"BotTab"} component={BotTab} /> */}
+      {/* <StatusBar barStyle='light-content' /> */}
+      {/* <StartScreen/> */}
+      {/* <BotTab /> */}
+
     </NavigationContainer>
   );
 }
