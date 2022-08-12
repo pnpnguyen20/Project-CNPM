f1dfb03af5a7eb0d28817930a2999d006ec78a4c
pnpnguyen20 <pnpnguyen20@clc.fitus.edu.vn>
2022-08-12 17:42:59 +0700

    sign up

--- frontend/screens/SignUpScreen.js
+++ frontend/screens/SignUpScreen.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { 
-    View, 
-    Text, 
-    TouchableOpacity, 
+import {
+    View,
+    Text,
+    TouchableOpacity,
     TextInput,
     Platform,
-    StyleSheet ,
+    StyleSheet,
     StatusBar,
     Alert
 } from 'react-native';
@@ -15,8 +15,10 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Feather from 'react-native-vector-icons/Feather';
 
 import { useTheme } from 'react-native-paper';
+import axios from '../components/axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 export const AuthContext = React.createContext();
-const SignInScreen = ({navigation}) => {
+const SignInScreen = ({ navigation }) => {
 
     const [data, setData] = React.useState({
         username: '',
@@ -27,23 +29,23 @@ const SignInScreen = ({navigation}) => {
         confirm_secureTextEntry: true,
     });
 
-     const { colors } = useTheme();
+    const { colors } = useTheme();
 
 
     const textInputChange = (val) => {
-        if( val.trim().length >= 5 ) {
+        if (val.trim().length >= 5) {
             setData({
                 ...data,
                 username: val,
                 check_textInputChange: true,
-                
+
             });
         } else {
             setData({
                 ...data,
                 username: val,
                 check_textInputChange: false,
-                
+
             });
         }
     }
@@ -67,7 +69,7 @@ const SignInScreen = ({navigation}) => {
             ...data,
             secureTextEntry: !data.secureTextEntry
         });
-        
+
     }
     const updateConfirmSecureTextEntry = () => {
         setData({
@@ -76,132 +78,160 @@ const SignInScreen = ({navigation}) => {
         });
     }
 
-    return (
-      <View style={styles.container}>
-          <StatusBar backgroundColor= '#528ae6' barStyle="light-content"/>
-        <View style={styles.header}>
-            <Text style={styles.text_header}>Create your new account!</Text>
-        </View>
-        <Animatable.View 
-            animation="fadeInUpBig"
-            style={[styles.footer, {
-                backgroundColor: colors.background
-            }]}
-        >
-            <Text style={[styles.text_footer, {
-                color: colors.text
-            }]}>Username</Text>
-            {<View style={styles.action}>
-                <FontAwesome 
-                    name="user-o"
-                    color={colors.text}
-                    size={20}
-                />
-                <TextInput 
-                    placeholder="Your username"
-                    placeholderTextColor="#666666"
-                    style={[styles.textInput, {
-                        color: colors.text
-                    }]}
-                    autoCapitalize="none"
-                    onChangeText={(val) => textInputChange(val)}
-                />
-                {data.check_textInputChange ? 
-                <Animatable.View animation="bounceIn">
-                
-                    <Feather 
-                        name="check-circle"
-                        color="blue"
-                        size={20}
-                    />
-                </Animatable.View>
-                : null} 
-            </View>
+    const trySignup = async (un, pw) => {
+        const { data } = await axios.post('/login',
+            {
+                "US_ID": 99,
+                "US_ACCOUNT": un,
+                "US_PASSWORD": pw,
+                "TOKEN": null
+            })
+        const message = data.message
+        if (message.success) {
+            return true
         }
-            
+        return false
 
-            <Text style={[styles.text_footer, {
-                color: colors.text,
-                marginTop: 35
-            }]}>Password</Text>
-            {<View style={styles.action}>
-                <Feather 
-                    name="lock"
-                    color={colors.text}
-                    size={20}
-                />
-                <TextInput 
-                    placeholder="Your password"
-                    placeholderTextColor="#666666"
-                    secureTextEntry={data.secureTextEntry ? true : false}
-                    style={[styles.textInput, {
-                        color: colors.text
-                    }]}
-                    autoCapitalize="none"
-                    onChangeText={(val) => handlePasswordChange(val)}
-                />
-                <TouchableOpacity onPress={updateSecureTextEntry}>
-                
-                    {data.secureTextEntry ?  
-                    <Feather 
-                        name="eye-off"
-                        color="grey"
+    }
+
+    const handleSignUp = async (un, pw) => {
+        if (await trySignup(un, pw)) {
+            AsyncStorage.setItem('un', un);
+            AsyncStorage.setItem('pw', pw);
+            navigation.navigate("BotTab")
+        }
+        else {
+            alert("Username is existed!!")
+        }
+
+    }
+
+    return (
+        <View style={styles.container}>
+            <StatusBar backgroundColor='#528ae6' barStyle="light-content" />
+            <View style={styles.header}>
+                <Text style={styles.text_header}>Create your new account!</Text>
+            </View>
+            <Animatable.View
+                animation="fadeInUpBig"
+                style={[styles.footer, {
+                    backgroundColor: colors.background
+                }]}
+            >
+                <Text style={[styles.text_footer, {
+                    color: colors.text
+                }]}>Username</Text>
+                {<View style={styles.action}>
+                    <FontAwesome
+                        name="user-o"
+                        color={colors.text}
                         size={20}
                     />
-                    :
-                    <Feather 
-                        name="eye"
-                        color="grey"
+                    <TextInput
+                        placeholder="Your username"
+                        placeholderTextColor="#666666"
+                        style={[styles.textInput, {
+                            color: colors.text
+                        }]}
+                        autoCapitalize="none"
+                        onChangeText={(val) => textInputChange(val)}
+                    />
+                    {data.check_textInputChange ?
+                        <Animatable.View animation="bounceIn">
+
+                            <Feather
+                                name="check-circle"
+                                color="blue"
+                                size={20}
+                            />
+                        </Animatable.View>
+                        : null}
+                </View>
+                }
+
+
+                <Text style={[styles.text_footer, {
+                    color: colors.text,
+                    marginTop: 35
+                }]}>Password</Text>
+                {<View style={styles.action}>
+                    <Feather
+                        name="lock"
+                        color={colors.text}
                         size={20}
                     />
-                    } 
-                </TouchableOpacity> 
-            </View>}
-            
-            <Text style={[styles.text_footer, {
-                color: colors.text,
-                marginTop: 35
-            }]}>Confirm password</Text>
-            <View style={styles.action}>
-                <Feather 
-                    name="lock"
-                    color={colors.text}
-                    size={20}
-                />
-                <TextInput placeholder="Confirm your password"
-                    placeholderTextColor="#666666"
-                    confirm_secureTextEntry={data.confirm_secureTextEntry ? true : false}
-                    style={[styles.textInput, {color: colors.text }]}
-                    autoCapitalize="none"
-                    onChangeText={(val) => handleConfirmPasswordChange(val)}
-                />
-                <TouchableOpacity onPress={updateConfirmSecureTextEntry}>
-                    {data.confirm_secureTextEntry ?  
-                    <Feather name="eye-off"color="grey"size={20}/>
-                    :
-                    <Feather name="eye" color="grey" size={20}/>
-                    } 
-                </TouchableOpacity> 
-            </View>
+                    <TextInput
+                        placeholder="Your password"
+                        placeholderTextColor="#666666"
+                        secureTextEntry={data.secureTextEntry ? true : false}
+                        style={[styles.textInput, {
+                            color: colors.text
+                        }]}
+                        autoCapitalize="none"
+                        onChangeText={(val) => handlePasswordChange(val)}
+                    />
+                    <TouchableOpacity onPress={updateSecureTextEntry}>
 
-            <View style={styles.textPrivate}>
-                <Text style={styles.color_textPrivate}>By signing up you agree to our</Text>
-                <Text style={[styles.color_textPrivate, {fontWeight: 'bold'}]}>{" "}Terms of service</Text>
-                <Text style={styles.color_textPrivate}>{" "}and</Text>
-                <Text style={[styles.color_textPrivate, {fontWeight: 'bold'}]}>{" "}Privacy policy</Text>
-            </View>
+                        {data.secureTextEntry ?
+                            <Feather
+                                name="eye-off"
+                                color="grey"
+                                size={20}
+                            />
+                            :
+                            <Feather
+                                name="eye"
+                                color="grey"
+                                size={20}
+                            />
+                        }
+                    </TouchableOpacity>
+                </View>}
 
-            <View style={styles.button}>
-                <TouchableOpacity style={[styles.signIn, {backgroundColor:'#528ae6'}]} onPress={() => {loginHandle( data.username, data.password )}}>
-                <Text style={[styles.textSign, {color: '#fff'}]}>Sign Up</Text>
-                </TouchableOpacity>
-            </View>
-            <View style={styles.signinTextCont}>
-					<Text style={styles.signinText}>Have an account?</Text>
-					<TouchableOpacity onPress={() => navigation.navigate('SignInScreen')}><Text style={[styles.textSign, {color: '#528ae6'}]}> Sign In</Text></TouchableOpacity>
-			</View>
-        </Animatable.View>
-      </View>
+                <Text style={[styles.text_footer, {
+                    color: colors.text,
+                    marginTop: 35
+                }]}>Confirm password</Text>
+                <View style={styles.action}>
+                    <Feather
+                        name="lock"
+                        color={colors.text}
+                        size={20}
+                    />
+                    <TextInput placeholder="Confirm your password"
+                        placeholderTextColor="#666666"
+                        confirm_secureTextEntry={data.confirm_secureTextEntry ? true : false}
+                        style={[styles.textInput, { color: colors.text }]}
+                        autoCapitalize="none"
+                        onChangeText={(val) => handleConfirmPasswordChange(val)}
+                    />
+                    <TouchableOpacity onPress={updateConfirmSecureTextEntry}>
+                        {data.confirm_secureTextEntry ?
+                            <Feather name="eye-off" color="grey" size={20} />
+                            :
+                            <Feather name="eye" color="grey" size={20} />
+                        }
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.textPrivate}>
+                    <Text style={styles.color_textPrivate}>By signing up you agree to our</Text>
+                    <Text style={[styles.color_textPrivate, { fontWeight: 'bold' }]}>{" "}Terms of service</Text>
+                    <Text style={styles.color_textPrivate}>{" "}and</Text>
+                    <Text style={[styles.color_textPrivate, { fontWeight: 'bold' }]}>{" "}Privacy policy</Text>
+                </View>
+
+                <View style={styles.button}>
+                    <TouchableOpacity style={[styles.signIn, { backgroundColor: '#528ae6' }]} onPress={() => { handleSignUp(data.username, data.password) }}>
+                        <Text style={[styles.textSign, { color: '#fff' }]}>Sign Up</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.signinTextCont}>
+                    <Text style={styles.signinText}>Have an account?</Text>
+                    <TouchableOpacity onPress={() => navigation.navigate('SignInScreen')}><Text style={[styles.textSign, { color: '#528ae6' }]}> Sign In</Text></TouchableOpacity>
+                </View>
+            </Animatable.View>
+        </View>
     );
 };
 
@@ -209,8 +239,8 @@ export default SignInScreen;
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1, 
-      backgroundColor: colors.primary1
+        flex: 1,
+        backgroundColor: colors.primary1
     },
     header: {
         flex: 1,
@@ -284,19 +314,19 @@ const styles = StyleSheet.create({
         color: 'grey'
     },
     signinText: {
-  	color: colors.placeholder,
-  	fontSize:16
-  },
-  signinButton: {
-  	color:'#ffffff',
-  	fontSize:16,
-  	fontWeight:'500'
-  },
-    signinTextCont : {
-  	flexGrow: 1,
-    alignItems:'flex-end',
-    justifyContent :'center',
-    paddingVertical:16,
-    flexDirection:'row'
-  },
-  });
+        color: colors.placeholder,
+        fontSize: 16
+    },
+    signinButton: {
+        color: '#ffffff',
+        fontSize: 16,
+        fontWeight: '500'
+    },
+    signinTextCont: {
+        flexGrow: 1,
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+        paddingVertical: 16,
+        flexDirection: 'row'
+    },
+});
